192509c8bbd4583de0a4198815e30fafaee67305
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-25 18:31:21 +0800

    Update byte.go

--- syntax/basic_type/byte.go
+++ syntax/basic_type/byte.go
@@ -6,5 +6,10 @@ func Byte() {
 
 	var a byte = 'a'
 	println(a)
-	fmt.Printf("%c", a)
+	fmt.Printf("%c\n", a)
+
+	var str string = "this is string"
+	var bs []byte = []byte(str)
+	bs[0] = 'T'
+	println(str, bs)
 }
