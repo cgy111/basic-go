7b81727345833dadf3bed946f580a8b3544916d8
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 17:32:01 +0800

    Update recursive.go

--- syntax/funcs/recursive.go
+++ syntax/funcs/recursive.go
@@ -1,5 +1,21 @@
 package main
 
-func Recursive() {
-	Recursive()
+func Recursive(n int) {
+	if n > 10 {
+		return
+	}
+
+	Recursive(n + 1)
+}
+
+func A() {
+	B()
+}
+
+func B() {
+	C()
+}
+
+func C() {
+	A()
 }
