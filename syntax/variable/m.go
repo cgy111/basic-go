afb8e266d4d8693e8a497850efbfcdc4deabd543
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 11:56:47 +0800

    Create m.go

---
+++ syntax/variable/m.go
@@ -0,0 +1,26 @@
+package main
+
+var Global = "全局变量"
+var interna = "私有变量"
+
+var (
+	First  string = "1"
+	second int    = 2
+)
+
+func main() {
+	var a int = 123
+	println(a)
+
+	var b = 234
+	println(b)
+
+	var c uint = 456
+	println(c)
+
+	var (
+		d string = "aaa"
+		e int    = 123
+	)
+	println(d, e)
+}
