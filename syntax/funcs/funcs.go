96c67003885c123f2a8e8d335f62a5dc0fc31d3d
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 16:59:26 +0800

    Update funcs.go

--- syntax/funcs/funcs.go
+++ syntax/funcs/funcs.go
@@ -19,3 +19,30 @@ func Func3(a int, b string) {
 func Func4(a, b string) {
 
 }
+
+// Func5多个参数,一个类型,另一个写法
+func Func5(a string, b string) {
+
+}
+
+func Func6(a, b string) string {
+	return "hello，world"
+}
+
+// 多个返回值
+func Func7(a, b string) (string, string) {
+	return "hello,world", "go"
+}
+
+func Func8() (name string, age int) {
+	return "cgy", 18
+}
+func Func9() (name string, age int) {
+	name = "cgy"
+	age = 18
+	return
+}
+func Func10() (name string, age int) {
+	//等价于 "",0
+	return
+}
