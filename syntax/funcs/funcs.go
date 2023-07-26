559c3458f7415c607ef148959c93520055b13e7f
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 16:53:04 +0800

    Create funcs.go

---
+++ syntax/funcs/funcs.go
@@ -0,0 +1,21 @@
+package funcs
+
+// Func1没有任何参数
+func Func1() {
+
+}
+
+// Func2有一个参数
+func Func2(a int) {
+
+}
+
+// Func3有多个参数
+func Func3(a int, b string) {
+
+}
+
+// Func4多个参数,一个类型
+func Func4(a, b string) {
+
+}
