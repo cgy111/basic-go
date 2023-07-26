607d3ecd76e222e958eb302605fd4be41632a2b8
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 18:51:44 +0800

    Update Defer.go

--- syntax/funcs/Defer.go
+++ syntax/funcs/Defer.go
@@ -8,3 +8,40 @@ func Defer() {
 		println("第二个defer")
 	}()
 }
+
+func DeferClosure() {
+	i := 0
+	defer func() {
+		println(i)
+	}()
+	i = 1
+}
+func DeferClosureV1() {
+	i := 0
+	defer func(val int) {
+		println(val)
+	}(i)
+	i = 1
+}
+
+/*func Query()  {
+	db := sql.Open("","")
+	defer db.Close()
+	db.Query("SELECT")
+}*/
+
+func DerferRetun() int {
+	a := 0
+	defer func() {
+		a = 1
+	}()
+	return a
+}
+
+func DeferReturnV1() (a int) {
+	a = 0
+	defer func() {
+		a = 1
+	}()
+	return a
+}
