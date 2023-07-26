b14313bbb54b3e99260a5fd7b63a469bd905bd97
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 18:24:16 +0800

    Update closure.go

--- syntax/funcs/closure.go
+++ syntax/funcs/closure.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 func Cloursre(name string) func() string {
 
 	return func() string {
 		return "hello," + name
 	}
+
+}
+func Cloursre2() func() string {
+	name := "cgy"
+	age := 18
+	return func() string {
+		return fmt.Sprintf("Hello,%s,%d", name, age)
+	}
+
+}
+func Cloursre3() func() int {
+	var age = 0
+	return func() int {
+		age++
+		return age
+	}
+
 }
