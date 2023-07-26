bf5263261bc347158468b63a31f3d92d1e98760f
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-26 17:26:58 +0800

    Update m.go

--- syntax/funcs/m.go
+++ syntax/funcs/m.go
@@ -2,15 +2,17 @@ package main
 
 func main() {
 
-	name, age := Func10()
-	println(name, age)
+	/*	name, age := Func10()
+		println(name, age)
 
-	name1, _ := Func10()
-	println(name1)
+		name1, _ := Func10()
+		println(name1)
 
-	//使用 := 的前提,就是左边必须有至少一个新变量
-	Func6("cgy", "hello")
-	name1, name2 := Func10()
-	println(name1)
-	println(name2)
+		//使用 := 的前提,就是左边必须有至少一个新变量
+		Func6("cgy", "hello")
+		name1, name2 := Func10()
+		println(name1)
+		println(name2)*/
+
+	Recursive()
 }
