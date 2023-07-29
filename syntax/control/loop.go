ef08227f363b94a0f2f125e8f052092a60963e9e
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-29 20:56:12 +0800

    Update loop.go

--- syntax/control/loop.go
+++ syntax/control/loop.go
@@ -35,3 +35,28 @@ func ForArr() {
 		println("下标 ", index, "值", arr[index])
 	}
 }
+func ForSlice() {
+	arr := []int{1, 2, 3}
+	for index, val := range arr {
+		println("下标 ", index, "值", val)
+	}
+	println()
+	for index := range arr {
+		println("下标 ", index, "值", arr[index])
+	}
+}
+
+func ForMap() {
+	m := map[string]int{
+		"key1": 100,
+		"key2": 102,
+	}
+
+	for k, v := range m {
+		println(k, v)
+	}
+	println()
+	for k := range m {
+		println(k, m[k])
+	}
+}
