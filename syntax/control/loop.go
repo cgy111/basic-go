1ede7a85aee6838ddebddca0e0b4b8b30bae75a0
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-29 20:49:42 +0800

    Update loop.go

--- syntax/control/loop.go
+++ syntax/control/loop.go
@@ -1,8 +1,37 @@
-package control
+package main
 
 func ForLoop() {
 	for i := 0; i < 10; i++ {
 		println(i)
 	}
-	
+	for i := 0; i < 10; {
+		println(i)
+		i++
+	}
+}
+func Loop2() {
+	i := 0
+	for i < 10 {
+		i++
+		println(i)
+	}
+	for true {
+		i++
+		println(i)
+	}
+	for {
+		i++
+		println(i)
+	}
+}
+
+func ForArr() {
+	arr := [3]int{1, 2, 3}
+	for index, val := range arr {
+		println("下标 ", index, "值", val)
+	}
+	println()
+	for index := range arr {
+		println("下标 ", index, "值", arr[index])
+	}
 }
