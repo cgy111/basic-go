3e31de78e020fc8649e71fdae5d27969ba2dc59c
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-31 18:03:04 +0800

    Update Slice.go

--- syntax/buildin_types/Slice.go
+++ syntax/buildin_types/Slice.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Slice() {
 	s1 := []int{9, 8, 7}
-	fmt.Printf("a1:%v,len=%d,cap=%d \n", s1, len(s1), cap(s1))
+	fmt.Printf("s1:%v,len=%d,cap=%d \n", s1, len(s1), cap(s1))
+	/*i := s1[0]
+	fmt.Printf(strconv.Itoa(i))*/
 
 	s2 := make([]int, 3, 4)
-	fmt.Printf("a2:%v,len=%d,cap=%d", s2, len(s2), cap(s2))
+	fmt.Printf("s2:%v,len=%d,cap=%d\n", s2, len(s2), cap(s2))
+
+	//{0,0,0,0}
+	s3 := make([]int, 4)
+	//make([]int,0,4)
+	fmt.Printf("s3:%v,len=%d,cap=%d\n", s3, len(s3), cap(s3))
+
+	s4 := make([]int, 0, 4)
+	s4 = append(s4, 1)
+	fmt.Printf("s4:%v,len=%d,cap=%d", s4, len(s4), cap(s4))
 }
