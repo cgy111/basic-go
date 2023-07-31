103a41760409c52f60797adff6756b0977143732
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-07-31 18:32:21 +0800

    Update Slice.go

--- syntax/buildin_types/Slice.go
+++ syntax/buildin_types/Slice.go
@@ -34,3 +34,18 @@ func SubSlice() {
 	s4 := s1[:3]
 	fmt.Printf("s4:%v,len=%d,cap=%d", s4, len(s4), cap(s4))
 }
+
+func ShareSlice() {
+	s1 := []int{1, 2, 3, 4}
+	s2 := s1[2:]
+	fmt.Printf("s2:%v,len=%d,cap=%d\n", s2, len(s2), cap(s2))
+
+	s2[0] = 99
+	fmt.Printf("s1:%v,len=%d,cap=%d \n", s1, len(s1), cap(s1))
+
+	s2 = append(s2, 199)
+
+	fmt.Printf("s2:%v,len=%d,cap=%d\n", s2, len(s2), cap(s2))
+	
+	fmt.Printf("s1:%v,len=%d,cap=%d \n", s1, len(s1), cap(s1))
+}
