96bdeb02f4ee354419a1be1841082bcdd1754bb9
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-08-01 18:14:05 +0800

    Update Defer.go

--- syntax/funcs/Defer.go
+++ syntax/funcs/Defer.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func Defer() {
 	defer func() {
 		println("第一个defer")
@@ -44,4 +46,31 @@ func DeferReturnV1() (a int) {
 		a = 1
 	}()
 	return a
+}
+
+func DeferClosureLoopV1() {
+	for i := 0; i < 10; i++ {
+		defer func() {
+			fmt.Printf("i的地址是%p,值是%d\n", &i, i)
+			//println(i)
+		}()
+	}
+}
+func DeferClosureLoopV2() {
+	for i := 0; i < 10; i++ {
+		defer func(val int) {
+			fmt.Printf("val的地址是%p,值是%d\n", &val, val)
+			//println(i)
+		}(i)
+	}
+}
+
+func DeferClosureLoopV3() {
+	for i := 0; i < 10; i++ {
+		j := i
+		defer func() {
+			fmt.Printf("j的地址是%p,值是%d\n", &j, j)
+			//println(i)
+		}()
+	}
 }
