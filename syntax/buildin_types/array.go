75046d9a14705ed4d5534e888e3dd97875645d7e
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-08-01 12:30:47 +0800

    Update array.go

--- syntax/buildin_types/array.go
+++ syntax/buildin_types/array.go
@@ -11,5 +11,26 @@ func Array() {
 
 	a3 := [3]int{}
 	fmt.Printf("a3:%v,len=%d,cap=%d 、\n", a3, len(a3), cap(a3))
+}
 
+func UseSumInt64() {
+
+	s1 := []int{1, 2, 3}
+	res := SumInt(s1)
+	println(res)
+}
+func SumInt(vals []int) int {
+	var res int
+	for _, val := range vals {
+		res += val
+	}
+	return res
+}
+
+func SumInt64(vals []int64) int64 {
+	var res int64
+	for _, val := range vals {
+		res += val
+	}
+	return res
 }
