b2f5d6e4d4c0b663bc2c252905249a1ca0b07dfe
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-08-08 18:23:07 +0800

    Create Demo.go

---
+++ syntax/another/Demo.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [4]int{10, 20, 30, 40}
+	fmt.Println(arr, len(arr), cap(arr))
+	s1 := arr[0:2]
+	fmt.Println(s1, len(s1), cap(s1))
+	s2 := s1
+	fmt.Println(s2, len(s2), cap(s2))
+	s3 := append(append(append(s1, 1), 2), 3)
+	fmt.Println(s3, len(s3), cap(s3))
+	s1[0] = 1000
+	fmt.Println(s1, len(s1), cap(s1))
+}
