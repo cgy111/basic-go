0d46e83a31ebd57c36ac70831f226d760eb8ab42
cgy111 <131836649+cgy111@users.noreply.github.com>
2023-08-09 22:06:30 +0800

    Update user.go

--- webook/intternal/web/user.go
+++ webook/intternal/web/user.go
@@ -9,12 +9,18 @@ type UserHandler struct {
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	//u := &UserHandler{}
 
-	server.POST("/users/signup", u.SignUp)
-	server.POST("/users/login", u.Login)
-	//非Rest风格
-	server.POST("/users/edit", u.Edit)
-	server.GET("users/profile", u.Profile)
+	/*	server.POST("/users/signup", u.SignUp)
+		server.POST("/users/login", u.Login)
+		//非Rest风格
+		server.POST("/users/edit", u.Edit)
+		server.GET("users/profile", u.Profile)*/
 
+	ug := server.Group("/users")
+
+	ug.GET("/profile", u.Profile)
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
 }
 
 type ArticleHandler struct {
