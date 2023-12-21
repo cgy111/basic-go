789018738286875d1626e0a40d7ec2ef00399068
cgy111 <17344031125@163.com>
2023-12-21 16:39:15 +0800

    l

--- script/wrk/profile.lua
+++ script/wrk/profile.lua
@@ -3,8 +3,28 @@
 --- Created by chenguangyao.
 --- DateTime: 2023/12/11 15:36
 ---
-wrk.method="GET"
+-- 初始token
+token = nil
+-- 需要请求验证的路由地址
+path = "/users/login"
+-- 第一次请求认证的请求方法
+method = "POST"
+
+-- 共有的headers设置
 wrk.headers["Content-Type"] = "application/json"
-wrk.headers["User-Agent"] = "PostmanRuntime/7.35.0"
--- 记得修改这个，你在登录页面登录一下，然后复制一个过来这里
-wrk.headers["Authorization"]="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDI0MzgzNTMsIlVpZCI6NCwiVXNlckFnZW50IjoiUG9zdG1hblJ1bnRpbWUvNy4zNS4wIn0.nLwwu57sgEsmO4WQDlOqpBIw5uwZdi-h_arEvQ2XEFLmayyahoo8vOC1AeiU7-e2SrNPmS06FzPT9tLL5huxgQ"
+wrk.headers["User-Agent"] = ""
+
+-- 发送第一次authenticate认证请求
+request = function ()
+    body = '{"email": "123o@qq.com","password": "hello@world123}'
+    return wrk.format(method, path, wrk.headers, body)
+end
+
+response = function (status, headers, body)
+    if not token and status == 200 then
+        token = headers["X-Jwt-Token"]
+        path = "/users/profile" -- 拿到token以后做资源地址的修改
+        method = "GET" -- 请求profile需要GET方法
+        wrk.headers["Authorization"] = string.format("Bear %s", token) -- 将获取到的token写入header中
+    end
+end
