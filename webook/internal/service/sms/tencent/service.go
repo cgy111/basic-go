821c361611abc814c35c8823b5218872862cedd7
cgy111 <131836649+cgy111@users.noreply.github.com>
2024-01-09 17:52:44 +0800

    limit

--- webook/internal/service/sms/tencent/service.go
+++ webook/internal/service/sms/tencent/service.go
@@ -27,17 +27,7 @@ func NewService(appId, signName string,
 }
 
 func (s *Service) SendTencent(ctx context.Context, tpl string, args []string, numbers ...string) error {
-	limited, err := s.limiter.Limit(ctx, "sms:tencent")
-	if err != nil {
-		//系统错误
-		//可以限流,保守策略,下游不给力的时候,
-		//可以不限流,你的下游很强,业务可用性要求很高,尽量容错策略
-		//包一下这个错误
-		return fmt.Errorf("短信服务判断是否限流出现问题,%w", err)
-	}
-	if limited {
-		return fmt.Errorf("触发了限流")
-	}
+
 	req := sms.NewSendSmsRequest()
 	req.SmsSdkAppId = s.appId
 	req.SignName = s.signName
