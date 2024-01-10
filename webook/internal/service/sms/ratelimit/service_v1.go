f2f114b6c833621422a346c6eceecc96fadd6a63
cgy111 <131836649+cgy111@users.noreply.github.com>
2024-01-10 13:40:33 +0800

    limit

---
+++ webook/internal/service/sms/ratelimit/service_v1.go
@@ -0,0 +1,39 @@
+package ratelimit
+
+//组合
+import (
+	"basic-go/webook/internal/service/sms"
+	"basic-go/webook/pkg/ratelimit"
+	"context"
+	"fmt"
+)
+
+type RatelimitSMSServiceV1 struct {
+	sms.Service
+	limiter ratelimit.Limiter
+}
+
+func NewRatelimitSMSServiceV1(svc sms.Service, limiter ratelimit.Limiter) sms.Service {
+	return &RatelimitSMSService{
+		svc:     svc,
+		limiter: limiter,
+	}
+}
+
+func (s *RatelimitSMSServiceV1) SendTencent(ctx context.Context, tpl string, args []string, numbers ...string) error {
+	limited, err := s.limiter.Limit(ctx, "sms:tencent")
+	if err != nil {
+		//系统错误
+		//可以限流,保守策略,下游不给力的时候,
+		//可以不限流,你的下游很强,业务可用性要求很高,尽量容错策略
+		//包一下这个错误
+		return fmt.Errorf("短信服务判断是否限流出现问题,%w", err)
+	}
+	if limited {
+		return errLimited
+	}
+	//在这里加一些代码
+	err = s.Service.SendTencent(ctx, tpl, args, numbers...)
+	//在这里也可以加一些代码，新特性
+	return err
+}
